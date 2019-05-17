1db6ae17a6b196a2fa6628fd5ac65e8a15c764e6
Zer1t0 <Zer1t0@mail.com>
2019-05-17 09:53:20 +0200

    implemented deref for sequence of

--- src/sequenceof.rs
+++ src/sequenceof.rs
@@ -2,11 +2,25 @@ use super::tag::{Tag};
 use super::traits::{Asn1Object, Asn1InstanciableObject, Asn1Tagged};
 use super::sequence::{SEQUENCE_TAG_NUMBER};
 use super::error::Asn1Error;
+use std::ops::{Deref, DerefMut};
 
 #[derive(Debug)]
 pub struct SequenceOf<T: Asn1InstanciableObject> {
     tag: Tag,
     components: Vec<T>
+}
+
+impl<T: Asn1InstanciableObject> Deref for SequenceOf<T> {
+    type Target = Vec<T>;
+    fn deref(&self) -> &Vec<T> {
+        &self.components
+    }
+}
+
+impl<T: Asn1InstanciableObject> DerefMut for SequenceOf<T> {
+    fn deref_mut(&mut self) -> &mut Vec<T> {
+        &mut self.components
+    }
 }
 
 impl<T: Asn1InstanciableObject> Asn1Tagged for SequenceOf<T> {
@@ -60,9 +74,6 @@ impl<T: Asn1InstanciableObject> SequenceOf<T> {
         };
     }
 
-    pub fn add(&mut self, item : T) {
-        self.components.push(item);
-    }
 }
 
 #[cfg(test)]
@@ -73,8 +84,8 @@ mod tests {
     #[test]
     fn test_encode_sequence_of_integers(){
         let mut seq_of: SequenceOf<Integer> = SequenceOf::new();
-        seq_of.add(Integer::new(9));
-        seq_of.add(Integer::new(1000));
+        seq_of.push(Integer::new(9));
+        seq_of.push(Integer::new(1000));
 
         assert_eq!(vec![0x30, 0x7, 
                         INTEGER_TAG_NUMBER, 0x1, 0x9, 
@@ -96,8 +107,8 @@ mod tests {
                         INTEGER_TAG_NUMBER, 0x1, 0x9, 
                         INTEGER_TAG_NUMBER, 0x2, 0x3, 0xe8]).unwrap();
 
-        assert_eq!(Integer::new(9), seq_of.components[0]);
-        assert_eq!(Integer::new(1000), seq_of.components[1]);
+        assert_eq!(Integer::new(9), seq_of[0]);
+        assert_eq!(Integer::new(1000), seq_of[1]);
 
     }
 
@@ -105,7 +116,7 @@ mod tests {
     fn test_decode_empty_sequence() {
         let mut seq_of: SequenceOf<Integer> = SequenceOf::new();
         seq_of.decode(&[0x30, 0x0]).unwrap();
-        assert_eq!(0, seq_of.components.len());
+        assert_eq!(0, seq_of.len());
     }
 
     #[test]
@@ -117,8 +128,8 @@ mod tests {
                                             0xff, 0xff]).unwrap();
         
         assert_eq!(9, consumed_octets);
-        assert_eq!(Integer::new(9), seq_of.components[0]);
-        assert_eq!(Integer::new(1000), seq_of.components[1]);
+        assert_eq!(Integer::new(9), seq_of[0]);
+        assert_eq!(Integer::new(1000), seq_of[1]);
     }
 
     #[should_panic(expected = "Invalid tag: Not valid tag for type")]
