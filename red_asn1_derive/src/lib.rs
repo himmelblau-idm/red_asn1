6c3b7bd436e49e4de2a968aa5b5981b4dffd54a8
Zer1t0 <Zer1t0@mail.com>
2019-08-06 07:05:06 +0200

    added docs to Sequence macro

--- red_asn1_derive/src/lib.rs
+++ red_asn1_derive/src/lib.rs
@@ -16,6 +16,58 @@ mod code_components;
 use parser::*;
 use code_components::*;
 
+
+/// Macro to create ASN1 sequence from Rust struct
+/// # Attributes
+/// 
+/// * seq: Define attributes for the struct definition. Has the following flags:
+///     + application_tag : Number of application tag
+/// 
+/// * seq_field: Define attributes for a field of the struct. Has the following flags:
+///     + context_tag: Number of context tag
+///     + optional: Flag to indicate that is an optional field
+/// 
+/// # Example
+/// 
+/// ```
+/// /*
+/// Person ::= [APPLICATION 1] SEQUENCE {
+///     name:       [0] GeneralString,
+///     age:        [1] Integer,
+///     address:    [2] GeneralString OPTIONAL,
+/// }
+/// */
+/// 
+/// use red_asn1::*;
+/// 
+/// #[derive(Sequence)]
+/// #[seq(application_tag = 1)]
+/// struct Person {
+///     #[seq_field(context_tag = 0)]
+///     name: SeqField<GeneralString>,
+///     #[seq_field(context_tag = 1)]
+///     age: SeqField<Integer>,
+///     #[seq_field(context_tag = 2, optional)]
+///     address: SeqField<GeneralString>
+/// }
+/// 
+/// let person = Person{
+///     name: GeneralString::from("John").into(),
+///     age: Integer::from(18).into(),
+///     address: SeqField::default()
+/// };
+/// 
+/// assert_eq!(
+///     vec![
+///         0x61, 0xf, 0x30, 0xd,
+///         0xa0, 0x6, 0x1b, 0x4, 0x4a, 0x6f, 0x68, 0x6e, // "John"
+///         0xa1, 0x3, 0x2, 0x1, 0x12 // 18
+///     ]
+///     , person.encode().unwrap()
+/// );
+/// 
+/// ```
+/// 
 #[proc_macro_derive(Sequence, attributes(seq, seq_field))]
 pub fn sequence_macro_derive(input: TokenStream) -> TokenStream {    
     let ast = parse_macro_input!(input as DeriveInput);
