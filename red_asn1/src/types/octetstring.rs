65a83eef70c2893735bd7dc81316514d3265c3c2
Eloy <zer1t0ps@protonmail.com>
2020-04-26 12:42:26 +0200

    adapt test in octetstring to return slice

--- red_asn1/src/types/octetstring.rs
+++ red_asn1/src/types/octetstring.rs
@@ -1,15 +1,13 @@
-use crate::tag::Tag;
-use crate::traits::*;
 use crate::error as asn1err;
+use crate::tag::Tag;
+use crate::traits::Asn1Object;
 
 pub static OCTET_STRING_TAG_NUMBER: u8 = 0x4;
-
 
 /// Class to build/parse OctetString ASN1
 pub type OctetString = Vec<u8>;
 
 impl Asn1Object for OctetString {
-
     fn tag() -> Tag {
         return Tag::new_primitive_universal(OCTET_STRING_TAG_NUMBER);
     }
@@ -22,7 +20,6 @@ impl Asn1Object for OctetString {
         *self = raw.to_vec();
         return Ok(());
     }
-
 }
 
 #[cfg(test)]
@@ -32,42 +29,68 @@ mod tests {
     #[test]
     fn test_build_octet_string() {
         assert_eq!(vec![0x4, 0x1, 0x0], OctetString::from(vec![0x0]).build());
-        assert_eq!(vec![0x04, 0x08, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef], 
-        OctetString::from(vec![0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef]).build());
+        assert_eq!(
+            vec![0x04, 0x08, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef],
+            OctetString::from(vec![
+                0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef
+            ])
+            .build()
+        );
         assert_eq!(vec![0x4, 0x0], OctetString::from(vec![]).build());
     }
 
     #[test]
     fn test_parse() {
-        assert_eq!(OctetString::from(vec![0x0]), _parse_octet_string(&[0x4, 0x1, 0x0]));
-        assert_eq!(OctetString::from(vec![0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef]),
-        _parse_octet_string(&[0x04, 0x08, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef]));
-        assert_eq!(OctetString::from(vec![]), _parse_octet_string(&[0x4, 0x0]));
+        assert_eq!(
+            OctetString::from(vec![0x0]),
+            OctetString::parse(&[0x4, 0x1, 0x0]).unwrap().1
+        );
+        assert_eq!(
+            OctetString::from(vec![
+                0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef
+            ]),
+            OctetString::parse(&[
+                0x04, 0x08, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef
+            ])
+            .unwrap()
+            .1
+        );
+        assert_eq!(
+            OctetString::from(vec![]),
+            OctetString::parse(&[0x4, 0x0]).unwrap().1
+        );
     }
 
     #[test]
     fn test_parse_with_excesive_bytes() {
-        assert_eq!((OctetString::from(vec![0x0]), 3), _parse_octet_string_with_consumed_octets(&[0x4, 0x1, 0x0,
-        0x01, 0x02, 0x03, 0x04]));
-        assert_eq!((OctetString::from(vec![0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef]), 0xa),
-        _parse_octet_string_with_consumed_octets(&[0x04, 0x08, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 
-        0x01, 0x02, 0x03, 0x04]));
-        assert_eq!((OctetString::from(vec![]), 2), _parse_octet_string_with_consumed_octets(&[0x4, 0x0,
-        0x01, 0x02, 0x03, 0x04]));
+        let x: &[u8] = &[0x1, 0x2, 0x3, 0x4];
+        assert_eq!(
+            (x, OctetString::from(vec![0x0])),
+            OctetString::parse(&[0x4, 0x1, 0x0, 0x01, 0x02, 0x03, 0x04])
+                .unwrap()
+        );
+        assert_eq!(
+            (
+                x,
+                OctetString::from(vec![
+                    0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef
+                ]),
+            ),
+            OctetString::parse(&[
+                0x04, 0x08, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+                0x01, 0x02, 0x03, 0x04
+            ])
+            .unwrap()
+        );
+        assert_eq!(
+            (x, OctetString::from(vec![])),
+            OctetString::parse(&[0x4, 0x0, 0x01, 0x02, 0x03, 0x04]).unwrap()
+        );
     }
 
-    #[should_panic (expected = "UnmatchedTag")]
+    #[should_panic(expected = "UnmatchedTag")]
     #[test]
     fn test_parse_with_invalid_tag() {
-        _parse_octet_string(&[0x7, 0x1, 0x0]);
-    }
-
-    fn _parse_octet_string(raw: &[u8]) -> OctetString {
-        return _parse_octet_string_with_consumed_octets(raw).0;
-    }
-
-    fn _parse_octet_string_with_consumed_octets(raw: &[u8]) -> (OctetString, usize) {
-        let (consumed_octets, os) = OctetString::parse(raw).unwrap();
-        return (os, consumed_octets);
+        OctetString::parse(&[0x7, 0x1, 0x0]);
     }
 }
