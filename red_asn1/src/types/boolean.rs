2402843ba638b797fc1ca7ea588b3cb25b94c8b0
Eloy <zer1t0ps@protonmail.com>
2020-04-26 12:50:03 +0200

    adapt boolean tests to return slice

--- red_asn1/src/types/boolean.rs
+++ red_asn1/src/types/boolean.rs
@@ -1,14 +1,13 @@
-use crate::tag::Tag;
-use crate::traits::*;
 use crate::error as asn1err;
+use crate::tag::Tag;
+use crate::traits::Asn1Object;
 
 pub static BOOLEAN_TAG_NUMBER: u8 = 0x1;
 
 /// Class to build/parse Boolean ASN1
 pub type Boolean = bool;
 
-impl Asn1Object for Boolean {
-
+impl Asn1Object for bool {
     fn tag() -> Tag {
         return Tag::new_primitive_universal(BOOLEAN_TAG_NUMBER);
     }
@@ -25,7 +24,6 @@ impl Asn1Object for Boolean {
         *self = raw[0] != 0;
         return Ok(());
     }
-
 }
 
 #[cfg(test)]
@@ -40,40 +38,42 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        assert_eq!(false, _parse(&[0x1, 0x1, 0x0]));
-        assert_eq!(true, _parse(&[0x1, 0x1, 0xff]));
-        assert_eq!(true, _parse(&[0x1, 0x1, 0x01]));
-        assert_eq!(true, _parse(&[0x1, 0x1, 0x7b]));
+        assert_eq!(false, bool::parse(&[0x1, 0x1, 0x0]).unwrap().1);
+        assert_eq!(true, bool::parse(&[0x1, 0x1, 0xff]).unwrap().1);
+        assert_eq!(true, bool::parse(&[0x1, 0x1, 0x01]).unwrap().1);
+        assert_eq!(true, bool::parse(&[0x1, 0x1, 0x7b]).unwrap().1);
     }
 
     #[test]
     fn test_parse_with_excesive_bytes() {
-        assert_eq!((false, 3), _parse_with_consumed_octets(&[0x1, 0x1, 0x0, 0x1]));
-        assert_eq!((true, 3), _parse_with_consumed_octets(&[0x1, 0x1, 0xff, 0x0, 0x1, 0x2]));
-        assert_eq!((true, 3), _parse_with_consumed_octets(&[0x1, 0x1, 0x01, 0x0, 0x1]));
-        assert_eq!((true, 3), _parse_with_consumed_octets(&[0x1, 0x1, 0x7b, 0x0]));
+        let x: &[u8] = &[0x0, 0x1];
+        assert_eq!((x, false), bool::parse(&[0x1, 0x1, 0x0, 0x1]).unwrap());
+        assert_eq!(
+            (x, true),
+            bool::parse(&[0x1, 0x1, 0xff, 0x0, 0x1]).unwrap()
+        );
+        assert_eq!(
+            (x, true),
+            bool::parse(&[0x1, 0x1, 0x01, 0x0, 0x1]).unwrap()
+        );
+        assert_eq!(
+            (x, true),
+            bool::parse(&[0x1, 0x1, 0x7b, 0x0, 0x1]).unwrap()
+        );
 
-        assert_eq!((false, 4), _parse_with_consumed_octets(&[0x1, 0x2, 0x0, 0x1]));
+        let y: &[u8] = &[];
+        assert_eq!((y, false), bool::parse(&[0x1, 0x2, 0x0, 0x1]).unwrap());
     }
 
-    #[should_panic (expected = "UnmatchedTag")]
+    #[should_panic(expected = "UnmatchedTag")]
     #[test]
     fn test_parse_with_invalid_tag() {
-        _parse(&[0x7, 0x1, 0x0]);
+        bool::parse(&[0x7, 0x1, 0x0]).unwrap();
     }
 
-    #[should_panic (expected = "NoDataForType")]
+    #[should_panic(expected = "NoDataForType")]
     #[test]
     fn test_parse_without_enough_value_octets() {
-        _parse(&[0x1, 0x0]);
-    }
-
-    fn _parse(raw: &[u8]) -> Boolean {
-        return _parse_with_consumed_octets(raw).0;
-    }
-
-    fn _parse_with_consumed_octets(raw: &[u8]) -> (Boolean, usize) {
-        let (consumed_octets, b) = Boolean::parse(raw).unwrap();
-        return (b, consumed_octets);
+        bool::parse(&[0x1, 0x0]).unwrap();
     }
 }
